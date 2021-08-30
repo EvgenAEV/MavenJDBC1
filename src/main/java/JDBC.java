7b325a6c99ca3a890b620ca962f437b01dcb9128
EvgenAEV <ageev.evgen.2020@mail.ru>
2021-08-30 16:51:00 +0300

    Initial commit

---
+++ src/main/java/JDBC.java
@@ -0,0 +1,32 @@
+import java.sql.*;
+
+public class JDBC {
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Kbrfy21kbrfy21kbrfy21java";
+    private static final String URL = "jdbc:mysql://localhost:3306/drinks";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    public static void main(String[] args) {
+        try {
+            Class.forName(DRIVER);
+            System.out.println("show");
+
+            Connection connection = DriverManager.getConnection(USERNAME, PASSWORD, URL);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM easy_drinks" );
+            while (resultSet.next()){
+                System.out.println(resultSet.getString(7));
+            }
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("F...k");
+        }
+    }
+
+
+}
+
