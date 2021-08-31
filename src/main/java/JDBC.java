3426694633fa679cf01e4ea14026cff2de828167
EvgenAEV <ageev.evgen.2020@mail.ru>
2021-08-31 16:20:38 +0300

    Initial commit

--- src/main/java/JDBC.java
+++ src/main/java/JDBC.java
@@ -1,26 +1,27 @@
 import java.sql.*;
 
 public class JDBC {
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "java";
-    private static final String URL = "jdbc:mysql://localhost:3306/mysql";
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static final String USER_NAME = "root";
+    public static final String PASSWORD = "java";
+    public static final String URL = "jdbc:mysql://localhost:3306/drinks";
+    public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static Connection connection;
+    public static Statement statement;
 
     public static void main(String[] args) {
         try {
             Class.forName(DRIVER);
             System.out.println("Show");
 
-            try (Connection connection = DriverManager.getConnection(USERNAME, PASSWORD, URL)) {
-                Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery("SELECT * FROM easy_drinks");
-                while (resultSet.next()) {
-                    System.out.println(resultSet.getString(7));
-                }
-                resultSet.close();
-                statement.close();
-
+            connection = DriverManager.getConnection(USER_NAME, PASSWORD, URL);
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM easy_drinks");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(7));
             }
+            resultSet.close();
+            statement.close();
+
         } catch (SQLException e) {
             System.out.println("НЕ работает");
             e.printStackTrace();
@@ -28,7 +29,8 @@ public class JDBC {
             System.out.println("No connection");
         }
     }
-
-
 }
+
+
+
 
